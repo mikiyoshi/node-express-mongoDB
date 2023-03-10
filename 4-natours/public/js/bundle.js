6d2ac4bbd03e3cf0826cc1239a5df93d46344e83
mikiyoshi <mikiyoshi.kokura@gmail.com>
2023-03-10 11:37:34 -0500

    replace deploy to railway.app

--- 4-natours/public/js/bundle.js
+++ 4-natours/public/js/bundle.js
@@ -11993,7 +11993,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58403" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53512" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
